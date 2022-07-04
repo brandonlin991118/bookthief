3d20345fbf56cda196330d150a61b01dc59d7d62
brandonlin991118 <80909833+brandonlin991118@users.noreply.github.com>
2022-07-04 14:24:55 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 //
 //  main.c
-//  bookthief
+//  hellom1pro
 //
-//  Created by Brandon Lin on 5/19/22.
-//  Copyright © 2022 Brandon Lin. All rights reserved.
+//  Created by Brandon Lin on 6/1/22.
 //
-#include<stdio.h>
-int main(void)
-{
-    int num=0;
-    while(1){
-        int sum=0;
-        int page=0;
-        scanf("%d",&num);
-        if(num==0) break;
-        for(int i=1;i<=num+1,sum<=num;i++){
-            sum+=i;
-            if(sum>num){
-                page=sum-num;
-                printf("%d %d\n",page,i);
-            }
+
+#include <stdio.h>
+#include <math.h>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int S ;
+    int N,i;
+    double p,q;
+    double a,r;
+    
+    scanf(" %d", &S);
+    while(S--){
+        scanf("%d %lf %d",&N,&p,&i);
+        q=1-p;
+        a=pow(q, i-1)*p;
+        r=pow(q,N);
+        if(p==0){
+            printf("0.0000\n");
         }
+        else{
+            printf("%4lf\n",(a*(1-pow(q, 100000))/(1-r)));
+        }
+    
     }
     return 0;
 }
-
